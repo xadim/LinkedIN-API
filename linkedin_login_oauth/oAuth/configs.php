114e6bd9b1a789636417be6120b60a72bfcdf261
xadim <xadimjaxate@gmail.com>
2014-04-29 12:27:08 -0400

    Create configs.php

---
+++ linkedin_login_oauth/oAuth/configs.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * Plugin Name: LinkedIN API
+ * Plugin URI: http://www.Teambuktoo.com/contact/
+ * Description: LinkedIN Api develped by Khadime Diakhate Teambuktoo
+ * Author: SkyVerge
+ * Author URI: http://www.Teambuktoo.com
+ * Version: 1.0
+ *
+ * Copyright: (c) 2013 Teambuktoo, Inc. (hello@teambuktoo.com)
+ *
+ * License: GNU General Public License v3.0
+ * License URI: http://www.gnu.org/licenses/gpl-3.0.html
+ *
+ * @author    Khadime Diakhate Teambuktoo
+ * @category  Custom
+ * @copyright Copyright (c) 2014, Teambuktoo, Inc.
+ * @license   http://www.gnu.org/licenses/gpl-3.0.html GNU General Public License v3.0
+ */
+
+$config['base_url']             =   'http://yourWebsite/'; 
+$config['callback_url']         =   'http://yourWebsite/';
+
+//linkedin configuration
+$config['linkedin_access']      =   'yourKeyFromLinkedIN';
+$config['linkedin_secret']      =   'yourSecretCodeFromLinkedIN'; 
+$config['linkedin_library_path']=   'linkedinoAuth.php';
+
+?>
